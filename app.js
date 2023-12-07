2c5576726854b0ad2f72026ff9581e9fdfd7b4a2
FrancisHuynh95 <huynh_francis95@icloud.com>
2023-12-06 23:47:12 -0800

    swapped portfolios

---
+++ app.js
@@ -0,0 +1,19 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+},
+{
+    threshold: .55,
+}
+)
+
+const hiddenElement = document.querySelectorAll(".hidden1")
+hiddenElement.forEach(element => observer.observe(element));
+
+
+// headerObserver.observe(hiddenElement)
